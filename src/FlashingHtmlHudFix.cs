342da82c0a6aa9c5a661852a5de1c78b8b7220f3
a4594865-crypto <a4594865@gmail.com>
2026-07-24 05:31:22 +0800

    Refactor FlashingHtmlHudFix module details
    Updated module version and author information.

--- src/FlashingHtmlHudFix.cs
+++ src/FlashingHtmlHudFix.cs
@@ -1,8 +1,6 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
-using CounterStrikeSharp.API.Modules.Commands;
-using System.Linq;
 
 namespace FlashingHtmlHudFix
 {
@@ -10,23 +8,17 @@ namespace FlashingHtmlHudFix
     public partial class FlashingHtmlHudFix : BasePlugin
     {
         public override string ModuleName => "FlashingHtmlHudFix";
-        public override string ModuleVersion => "1.2_Toggleable";
-        public override string ModuleAuthor => "Deana (Modified for LiteMatch)";
-        public override string ModuleDescription => "A Plugin that fixes Html Hud. (Toggleable Switch)";
+        public override string ModuleVersion => "1.1";
+        public override string ModuleAuthor => "Deana https://x.com/girlglock_/";
+        public override string ModuleDescription => "A Plugin that fixes Html Hud.";
 
         private CCSGameRules? _gameRules;
         private bool _gameRulesInitialized;
-
-        // 【新增】：防閃機制的總開關，預設為「關閉 (false)」
-        public bool IsFixActive = false;
 
         public override void Load(bool hotReload)
         {
             RegisterListener<Listeners.OnTick>(OnTick);
             RegisterListener<Listeners.OnMapStart>(OnMapStartHandler);
-
-            // 【新增】：註冊一個給 LiteMatchManager 呼叫的遙控器指令
-            AddCommand("css_hud_fix_toggle", "Toggle HTML fix", OnToggleCommand);
 
             if (hotReload)
             {
@@ -34,22 +26,10 @@ namespace FlashingHtmlHudFix
             }
         }
 
-        private void OnToggleCommand(CCSPlayerController? player, CommandInfo info)
-        {
-            // 限制只能由伺服器後台呼叫，玩家不能用
-            if (player != null) return; 
-
-            if (info.ArgCount >= 2)
-            {
-                IsFixActive = info.GetArg(1) == "1";
-            }
-        }
-
         private void OnMapStartHandler(string mapName)
         {
             _gameRules = null;
             _gameRulesInitialized = false;
-            IsFixActive = false; // 換地圖時自動重置開關
         }
 
         private void InitializeGameRules()
@@ -63,9 +43,6 @@ namespace FlashingHtmlHudFix
 
         private void OnTick()
         {
-            // 【核心改動】：如果開關沒開，就直接跳出，把畫面控制權還給官方！
-            if (!IsFixActive) return;
-
             if (!_gameRulesInitialized)
             {
                 InitializeGameRules();
